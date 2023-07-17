09850912a024a4dc079a08d69df52cb61e83c3f3
skwilliams <williamskstacy@gmail.com>
2023-07-17 02:25:48 -0400

    featured depart

--- src/components/PageComponents/Featured.js
+++ src/components/PageComponents/Featured.js
@@ -1,37 +1,33 @@
+
 import React from 'react'
 import FeatStyle from '../../styles/Featured.module.scss'
 import Link from "next/link"
 import Image from 'next/image'
 
-const Featured = (photo,altText,title,text,link) => {
+const Featured = (photo,altText,title,text,goto) => {
   return (
     <>
-  
-    <div className={FeatStyle.gridContwo}>
+      <div className={FeatStyle.gridContwo}>
         <div className={FeatStyle.gridtwo}>
-
-            <Image className={FeatStyle.imgstyle} src={photo} alt={altText} />
-               
-
+          <Image
+            className={FeatStyle.imgstyle}
+            src={photo}
+          
+            alt={altText}
+           
+            width="200"
+            height="200"
+            // margin="20px"
+          />
         </div>
-         <div className={FeatStyle.gridtwo}>
+        <div className={FeatStyle.gridtwo}>
+          <div className={FeatStyle.title}>{title}</div>
 
-            <div className={FeatStyle.title}>{title}</div> 
-           
-             <p className={FeatStyle.text}> {text}</p> 
-             <Link className={FeatStyle.goto}>View More... href={link} </Link>
+          <p className={FeatStyle.text}> {text}</p>
+          {/* <Link className={FeatStyle.goto} href={goto}>  View More...  </Link> */}
         </div>
-</div>
-
-        
-
-
-    
-        
-           
-           
-        </>      
-
+      </div>
+    </>
   );
 }
 
