73eb64791fcd4215d05cf3d103fb553336a78721
adnagee <8933165+adnagee@users.noreply.github.com>
2023-07-27 13:53:48 -0400

    updated javascript file

--- src/pages/programmes/schools/schooldata.js
+++ src/pages/programmes/schools/schooldata.js
@@ -3,27 +3,28 @@ export const schools = [
     name: "School of Business and Digital Technologies",
     shortName: "Business & Digital Technologies",
     nameStump: "busIT",
+    overview:
+      "School of Business and Digital Technologies overview. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nemo sed quae repellat blanditiis corrupti rerum quia voluptatem dolores, explicabo eius numquam, dolore incidunt reprehenderit aperiam, ad eos veniam unde. We are here for you Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia distinctio voluptatem a nemo quam, blanditiis quasi ipsa? Architecto aspernatur, esse eius id ut accusamus nihil ipsam nemo necessitatibus praesentium! Dolorem.",
     dean: {
-      name: "Lalita Ramlal-Chirckoot",
-      image: require("../../../../../images/Links/home1.png"),
+      name: "Vacant",
+      image: "../../../../images/Programmes/deanavatar.jpg",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat possimus, at iure soluta sequi eaque doloribus ex fugit aliquid quidem dolores non nisi optio adipisci dolore numquam laudantium quis.",
-      email: "ltchirkoot@costaatt.edu.tt",
+        "As the dean of Business & Digital Technologies, Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat possimus, at iure soluta sequi eaque doloribus ex fugit aliquid quidem dolores non nisi optio adipisci dolore numquam laudantium quis.",
+      email: "info@costaatt.edu.tt",
       phone: "868-625-5030 ext 1111",
     },
-    overview:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nemo sed quae repellat blanditiis corrupti rerum quia voluptatem dolores, explicabo eius numquam, dolore incidunt reprehenderit aperiam, ad eos veniam unde. We are here for you Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia distinctio voluptatem a nemo quam, blanditiis quasi ipsa? Architecto aspernatur, esse eius id ut accusamus nihil ipsam nemo necessitatibus praesentium! Dolorem.",
+
     departments: [
       {
         name: "Business",
-        image: "../../../../../images/Links/home2.png",
+        image: "/images/schools/busIT.png",
         summary:
           "From entrepreneurs and business professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
         shortName: "Business",
       },
       {
         name: "Digital Technologies",
-        image: "../../../../../images/Links/home2.png",
+        image: "/images/schools/busIT.png",
         summary:
           "From entrepreneurs and business professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
         shortName: "IST",
@@ -35,115 +36,75 @@ export const schools = [
     shortName: "Liberal Arts",
     nameStump: "liberal",
     dean: {
-      name: "Lalita Ramlal-Chirckoot",
-      image: "../../../../../images/Links/home1.png",
+      name: "Neil Sylvester",
+      image: "../../../../images/Programmes/deanavatar.jpg",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat possimus, at iure soluta sequi eaque doloribus ex fugit aliquid quidem dolores non nisi optio adipisci dolore numquam laudantium quis.",
-      email: "ltchirkoot@costaatt.edu.tt",
-      phone: "868-625-5030 ext 1111",
+        "As the dean of School of Liberal Arts, Education and Digital Humanities, Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat possimus, at iure soluta sequi eaque doloribus ex fugit aliquid quidem dolores non nisi optio adipisci dolore numquam laudantium quis.",
+      email: "nsylvester@costaatt.edu.tt",
+      phone: "868-625-5030 ext 2222",
     },
     overview:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nemo sed quae repellat blanditiis corrupti rerum quia voluptatem dolores, explicabo eius numquam, dolore incidunt reprehenderit aperiam, ad eos veniam unde. We are here for you Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia distinctio voluptatem a nemo quam, blanditiis quasi ipsa? Architecto aspernatur, esse eius id ut accusamus nihil ipsam nemo necessitatibus praesentium! Dolorem.",
+      "School of Liberal Arts, Education and Digital Humanities overview. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nemo sed quae repellat blanditiis corrupti rerum quia voluptatem dolores, explicabo eius numquam, dolore incidunt reprehenderit aperiam, ad eos veniam unde. We are here for you Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia distinctio voluptatem a nemo quam, blanditiis quasi ipsa? Architecto aspernatur, esse eius id ut accusamus nihil ipsam nemo necessitatibus praesentium! Dolorem.",
     departments: [
       {
-        name: "Business",
-        image: "../../../../../images/Links/home2.png",
+        name: "Education",
+        image: "/images/schools/liberal.png",
         summary:
-          "From entrepreneurs and business professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
-        shortName: "Business",
+          "From language experts and mathematics professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
+        shortName: "Education",
       },
       {
-        name: "Digital Technologies",
-        image: "../../../../../images/Links/home2.png",
+        name: "Criminal Justice, Human Security and Legal Studies",
+        image: "/images/schools/liberal.png",
+        summary:
+          "From protective service professionals, paralegal professionals, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
+        shortName: "Criminal Justice",
+      },
+      {
+        name: "Social and Behavioural Sciences",
+        image: "/images/schools/liberal.png",
+        summary:
+          "From Social workers, Therapists and Counselling professionals, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
+        shortName: "Social and Behavioural Sciences",
+      },
+    ],
+  },
+  {
+    name: "School of Nursing, Health and Medical Technologies",
+    shortName: "Nursing and Allied Health",
+    nameStump: "nursing",
+    dean: {
+      name: "Anjenney Boodoosingh-Dwarika",
+      image: "../../../../images/Programmes/deanavatar.jpg",
+      message:
+        "As the dean of School of Nursing, Health and Medical Technologies, Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat possimus, at iure soluta sequi eaque doloribus ex fugit aliquid quidem dolores non nisi optio adipisci dolore numquam laudantium quis.",
+      email: "adwarika@costaatt.edu.tt",
+      phone: "868-625-5030 ext 3333",
+    },
+    overview:
+      "School of Nursing, Health and Medical Technologies overview. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nemo sed quae repellat blanditiis corrupti rerum quia voluptatem dolores, explicabo eius numquam, dolore incidunt reprehenderit aperiam, ad eos veniam unde. We are here for you Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia distinctio voluptatem a nemo quam, blanditiis quasi ipsa? Architecto aspernatur, esse eius id ut accusamus nihil ipsam nemo necessitatibus praesentium! Dolorem.",
+    departments: [
+      {
+        name: "Nursing",
+        image: "/images/schools/nursing.png",
+        summary:
+          "From Nurses, nurses aides,  this department is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
+        shortName: "Nursing",
+      },
+      {
+        name: "Health and Medical Science Technologies",
+        image: "/images/schools/nursing.png",
         summary:
           "From entrepreneurs and business professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
-        shortName: "ISDT",
+        shortName: "HMST",
+      },
+      {
+        name: "Public Health and Wellness",
+        image: "/images/schools/nursing.png",
+        summary:
+          "From entrepreneurs and business professionals to information technology enthusiasts, this school is for you! Students at this school will be able to connect classroom content, to real world application. You will be equipped with the skills and support for success.",
+        shortName: "Health and Wellness",
       },
     ],
   },
 ];
-//   {
-//     name: "",
-//     shortName: "",
-// nameStump : "",
-//     dean: {
-//       name: "",
-//       image: "",
-//       message: "",
-//       email: "",
-//       phone: "",
-//     },
-//     overview: "",
-//     departments: [
-//       {
-//         name: "",
-//         image: "",
-//         summary: "",
-//         shortName: "",
-//       },
-//     ],
-//   },
-
-//   {
-//     name: "",
-//     shortName: "",
-//     dean: {
-//       name: "",
-//       image: "",
-//       message: "",
-//       email: "",
-//       phone: "",
-//     },
-//     overview: "",
-//     departments: [
-//       {
-//         name: "",
-//         image: "",
-//         summary: "",
-//         shortName: "",
-//       },
-//     ],
-//   },
-
-//   {
-//     name: "",
-//     shortName: "",
-//     dean: {
-//       name: "",
-//       image: "",
-//       message: "",
-//       email: "",
-//       phone: "",
-//     },
-//     overview: "",
-//     departments: [
-//       {
-//         name: "",
-//         image: "",
-//         summary: "",
-//         shortName: "",
-//       },
-//     ],
-//   },
-
-//   {
-//     name: "",
-//     shortName: "",
-//     dean: {
-//       name: "",
-//       image: "",
-//       message: "",
-//       email: "",
-//       phone: "",
-//     },
-//     overview: "",
-//     departments: [
-//       {
-//         name: "",
-//         image: "",
-//         summary: "",
-//         shortName: "",
-//       },
-//     ],
-//   },
-// ];
