525964ad8caa08059f34005908c83b6bb250d88b
skwilliams <williamskstacy@gmail.com>
2023-08-18 16:32:07 -0400

    how to pay page

--- src/pages/admissions/howtopay.js
+++ src/pages/admissions/howtopay.js
@@ -16,7 +16,7 @@ const howtopay = () => {
         <title> Fee Payment Options </title>
       </Head>
       {/* <HeaderNoImage caption="How to Pay " /> */}
-      <p className=''> Fee Payment Options</p>
+      <p className={AdminStyles.sectiontextleft}> Fee Payment Options</p>
       <section id="beforepay" className={AdminStyles.sectionIntro}>
         <p className={AdminStyles.text}>
           {" "}
@@ -27,30 +27,38 @@ const howtopay = () => {
           <ul className={AdminStyles.list}>
             <li className={AdminStyles.items}>Your 8-digit student ID</li>
             <li className={AdminStyles.items}> Amount to be paid</li>
-            <li className={AdminStyles.items}> Fee Code </li> <Link> Place link to pdf Fee Code here</Link>
+            <li className={AdminStyles.items}>
+              {" "}
+              Fee Code{" "}
+              <span>
+                {" "}
+                <Link href="#" className={AdminStyles.emphasis}>
+                  {" "}
+                  Place link to pdf Fee Code here
+                </Link>{" "}
+              </span>
+            </li>
             <li className={AdminStyles.items}>
               Relevant term/semester code e.g.202410{" "}
             </li>
           </ul>
         </div>
       </section>
-      <section id="visitcashier" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> Visit the Cashier</p>
+      <section id="visitcashier" className={AdminStyles.viewcashier}>
+        <p className={AdminStyles.sectiontextleft}> Visit the Cashier</p>
       </section>
-      <section id="banneronline" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> Visit the Cashier</p>
-      </section>
-      <section id="personalonline" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> Personal Online</p>
+      
+      <section id="personalonline" className={AdminStyles.payomline}>
+        <p className={AdminStyles.sectiontextleft}> Personal Online</p>
       </section>
       <section id="interbank" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> Inter Bank </p>
+        <p className={AdminStyles.sectiontextleft}> Inter Bank </p>
       </section>
       <section id="directdeposit" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> Direct Deposit</p>
+        <p className={AdminStyles.sectiontextleft}> Direct Deposit</p>
       </section>
       <section id="afterpay" className={AdminStyles.pay}>
-        <p className={AdminStyles.header}> After you Pay</p>
+        <p className={AdminStyles.sectiontextleft}> After you Pay</p>
         <p className={AdminStyles.text}>
           {" "}
           After you pay it's important that you do the following:
