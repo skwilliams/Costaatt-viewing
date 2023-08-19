c9964e910cd234f2f9538f2422c579b1e9341cfd
Jamal Emmanuel <jamal.emmanuel23@gmail.com>
2023-08-18 23:06:35 -0400

    layout change how to pay

--- src/pages/admissions/howtopay.js
+++ src/pages/admissions/howtopay.js
@@ -20,75 +20,77 @@ const howtopay = () => {
       <Head>
         <title> Fee Payment Options </title>
       </Head>
-      {/* <HeaderNoImage caption="How to Pay " /> */}
-      <p className={AdminStyles.sectiontextleft}> Fee Payment Options</p>
-      <section id="beforepay" className={AdminStyles.sectionIntro}>
-        <p className={AdminStyles.text}>
-          {' '}
-          <Image className={CourseStyles.icon} href={before} width="70" />
-          Before you pay ensure you are in possesion of the following:
-        </p>
-        <div className={AdminStyles.entryreq}>
-          <ul className={AdminStyles.list}>
-            <li className={AdminStyles.items}>Your 8-digit student ID</li>
-            <li className={AdminStyles.items}> Amount to be paid</li>
-            <li className={AdminStyles.items}>
-              {' '}
-              Fee Code{' '}
-              <span>
+      <HeaderNoImage caption="How to Pay " />
+      <SideNavOnlyLayout>
+        <p className={AdminStyles.sectiontextleft}> Fee Payment Options</p>
+        <section id="beforepay" className={AdminStyles.sectionIntro}>
+          <p className={AdminStyles.text}>
+            {' '}
+            <Image className={CourseStyles.icon} href={before} width="70" />
+            Before you pay ensure you are in possesion of the following:
+          </p>
+          <div className={AdminStyles.entryreq}>
+            <ul className={AdminStyles.list}>
+              <li className={AdminStyles.items}>Your 8-digit student ID</li>
+              <li className={AdminStyles.items}> Amount to be paid</li>
+              <li className={AdminStyles.items}>
                 {' '}
-                <Link href="#" className={AdminStyles.emphasis}>
+                Fee Code{' '}
+                <span>
                   {' '}
-                  Place link to pdf Fee Code here
-                </Link>{' '}
-              </span>
-            </li>
-            <li className={AdminStyles.items}>
-              Relevant term/semester code e.g.202410{' '}
-            </li>
-          </ul>
-        </div>
-      </section>
-      <section id="visitcashier" className={AdminStyles.viewcashier}>
-        <p className={AdminStyles.sectiontextleft}> Visit the Cashier</p>
-      </section>
+                  <Link href="#" className={AdminStyles.emphasis}>
+                    {' '}
+                    Place link to pdf Fee Code here
+                  </Link>{' '}
+                </span>
+              </li>
+              <li className={AdminStyles.items}>
+                Relevant term/semester code e.g.202410{' '}
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section id="visitcashier" className={AdminStyles.viewcashier}>
+          <p className={AdminStyles.sectiontextleft}> Visit the Cashier</p>
+        </section>
 
-      <section id="personalonline" className={AdminStyles.payomline}>
-        <p className={AdminStyles.sectiontextleft}> Personal Online</p>
-      </section>
-      <section id="interbank" className={AdminStyles.pay}>
-        <p className={AdminStyles.sectiontextleft}> Inter Bank </p>
-      </section>
-      <section id="directdeposit" className={AdminStyles.pay}>
-        <p className={AdminStyles.sectiontextleft}> Direct Deposit</p>
-      </section>
-      <section id="afterpay" className={AdminStyles.pay}>
-        <p className={AdminStyles.sectiontextleft}> After you Pay</p>
-        <p className={AdminStyles.text}>
-          {' '}
-          After you pay it's important that you do the following:
-        </p>
-        <div className={AdminStyles.entryreq}>
-          <ul className={AdminStyles.list}>
-            <li className={AdminStyles.items}>
-              Retain a print/photograp/email of payment receipt
-            </li>
-            <li className={AdminStyles.items}>
-              {' '}
-              Send to{' '}
-              <a href="mailto:bursar@costaatt.edu.tt">
+        <section id="personalonline" className={AdminStyles.payomline}>
+          <p className={AdminStyles.sectiontextleft}> Personal Online</p>
+        </section>
+        <section id="interbank" className={AdminStyles.pay}>
+          <p className={AdminStyles.sectiontextleft}> Inter Bank </p>
+        </section>
+        <section id="directdeposit" className={AdminStyles.pay}>
+          <p className={AdminStyles.sectiontextleft}> Direct Deposit</p>
+        </section>
+        <section id="afterpay" className={AdminStyles.pay}>
+          <p className={AdminStyles.sectiontextleft}> After you Pay</p>
+          <p className={AdminStyles.text}>
+            {' '}
+            After you pay it's important that you do the following:
+          </p>
+          <div className={AdminStyles.entryreq}>
+            <ul className={AdminStyles.list}>
+              <li className={AdminStyles.items}>
+                Retain a print/photograp/email of payment receipt
+              </li>
+              <li className={AdminStyles.items}>
                 {' '}
-                bursar@costaatt.edu.tt
-              </a>
-            </li>
-            <li className={AdminStyles.items}>
-              {' '}
-              Check your student email for confirmation that the payment was
-              received and processed{' '}
-            </li>
-          </ul>
-        </div>
-      </section>
+                Send to{' '}
+                <a href="mailto:bursar@costaatt.edu.tt">
+                  {' '}
+                  bursar@costaatt.edu.tt
+                </a>
+              </li>
+              <li className={AdminStyles.items}>
+                {' '}
+                Check your student email for confirmation that the payment was
+                received and processed{' '}
+              </li>
+            </ul>
+          </div>
+        </section>
+      </SideNavOnlyLayout>
     </>
   );
 };
