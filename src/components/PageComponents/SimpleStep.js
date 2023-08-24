708fbeb731f915bbfc642429825392d75e2e5abf
skwilliams <williamskstacy@gmail.com>
2023-08-24 10:17:01 -0400

    steps

--- src/components/PageComponents/SimpleStep.js
+++ src/components/PageComponents/SimpleStep.js
@@ -4,30 +4,31 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Link from 'next/link'
+import AdmisStyles from "@/styles/Admissions.module.scss"
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const steps = [
-    {
-        label:"Apply to College",
-        linkto:"applynow"
-    },
-    {
-        label:"Complete Transfer From",
-        linkto:""
-    },
-    {
-        label:"Submit to Admission",
-        linkto:""
-    },
+  {
+    label: "Apply to College",
+    linkto: "applynow",
+  },
+  {
+    label: "Download and Complete your Appliation for Transfer",
+    linkto: "",
+  },
+  
 ];
 
 export default function SimpleStep() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper  alternativeLabel>
+    <Box sx={{ width: "100%" }} >
+      <Stepper alternativeLabel>
         {steps.map((label) => (
           <Step key={label.label}>
             <Link href={label.linkto}>
-            <StepLabel>{label.label}</StepLabel>
+              <StepLabel className={AdmisStyles.stepicon} StepIconComponent={SettingsIcon}>
+                {label.label}
+              </StepLabel>
             </Link>
           </Step>
         ))}
