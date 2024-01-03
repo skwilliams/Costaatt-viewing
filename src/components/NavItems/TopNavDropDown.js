7d2b13ab1e369cd859a57ffbf43b50f40715c8e9
Anthony <abtrini@gmail.com>
2024-01-02 22:23:38 -0400

    Navbar Changes...work in progress

--- src/components/NavItems/TopNavDropDown.js
+++ src/components/NavItems/TopNavDropDown.js
@@ -1,28 +1,38 @@
 // TopNavDropDown.js
+import React, { useState } from "react";
 import navStyles from "../../styles/NavItems.module.scss";
 import Link from "next/link";
 
-const TopNavDropDown = ({ items }) => {
+const TopNavDropDown = ({ items, closeDropdown }) => {
+  const handleLinkClick = () => {
+    closeDropdown(); // Close the dropdown when a link is clicked
+  };
+
   return (
-    <div className={navStyles.topNavDropDownMain}>
-      <div className={navStyles.topNavdropDownMenu}>
-        {items.submenu &&
-          items.submenu.map((submenu, index) => (
-            <div key={index} className={navStyles.topNavdropDownSchoolMenu}>
-              <div className={navStyles.topNavdropDownSchoolLink}>
-                {submenu.title}
-              </div>
-              {submenu.submenu && (
-                <div className={navStyles.topNavdropDownDeptLink}>
-                  {submenu.submenu.map((subSubMenu, subIndex) => (
-                    <Link href={subSubMenu.path} key={subIndex}>
-                      {subSubMenu.title}
-                    </Link>
-                  ))}
+    <div className={navStyles.topNavDropDownContainer}>
+      <div className={navStyles.topNavDropDownMain}>
+        <div className={navStyles.topNavdropDownMenu}>
+          {items.submenu &&
+            items.submenu.map((submenu, index) => (
+              <div key={index} className={navStyles.topNavdropDownSchoolMenu}>
+                <div className={navStyles.topNavdropDownSchoolLink}>
+                  <Link href={submenu.path} passHref>
+                    {/* Attach an onClick event to close the dropdown */}
+                    <div onClick={handleLinkClick}>{submenu.title}</div>
+                  </Link>
                 </div>
-              )}
-            </div>
-          ))}
+                {submenu.submenu && (
+                  <div className={navStyles.topNavdropDownDeptLink}>
+                    {submenu.submenu.map((subSubMenu, subIndex) => (
+                      <Link href={subSubMenu.path} key={subIndex}>
+                        <div onClick={handleLinkClick}>{subSubMenu.title}</div>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
