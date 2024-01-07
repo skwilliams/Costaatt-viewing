aa9dede34134e121b4b00020fa956c85acaf5e3d
Anthony <abtrini@gmail.com>
2024-01-07 19:18:49 -0400

    Change to dropdown

--- src/components/NavComponents/DropDownItems.js
+++ src/components/NavComponents/DropDownItems.js
@@ -1,31 +1,31 @@
-// TopNavItems.js
-import React, { useState } from "react";
-import { FaAngleDown, FaAngleUp } from "react-icons/fa";
-import navStyles from "../../styles/NavItems.module.scss";
-import Link from "next/link";
+// // TopNavItems.js
+// import React, { useState } from "react";
+// import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+// import navStyles from "../../styles/NavItems.module.scss";
+// import Link from "next/link";
 
-const TopNavItems = ({ items, toggleClick }) => {
-  const [dropdown, setDropdown] = useState(false);
-  const handleClick = () => {
-    setDropdown(!dropdown);
-    toggleClick(items.title);
-  };
-  return (
-    <div className={navStyles.topBarLinks}>
-      {items.submenu ? (
-        <>
-          <div onClick={handleClick}>
-            <Link href={"#"}>{items.title}</Link>
-            {dropdown ? <FaAngleUp /> : <FaAngleDown />}{" "}
-          </div>
-        </>
-      ) : (
-        <div>
-          <Link href={items.path}>{items.title}</Link>
-        </div>
-      )}
-    </div>
-  );
-};
+// const TopNavItems = ({ items, toggleClick }) => {
+//   const [dropdown, setDropdown] = useState(false);
+//   const handleClick = () => {
+//     setDropdown(!dropdown);
+//     toggleClick(items.title);
+//   };
+//   return (
+//     <div className={navStyles.topBarLinks}>
+//       {items.submenu ? (
+//         <>
+//           <div onClick={handleClick}>
+//             <Link href={"#"}>{items.title}</Link>
+//             {dropdown ? <FaAngleUp /> : <FaAngleDown />}{" "}
+//           </div>
+//         </>
+//       ) : (
+//         <div>
+//           <Link href={items.path}>{items.title}</Link>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-export default TopNavItems;
+// export default TopNavItems;
