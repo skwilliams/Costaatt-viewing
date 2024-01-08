c2b96c2116270fdabf9a5ae1c25a30d3775e8b3b
skwilliams <williamskstacy@gmail.com>
2024-01-08 06:57:30 -0400

    all programmes

--- src/pages/programmes/allprogrammes.js
+++ src/pages/programmes/allprogrammes.js
@@ -13,7 +13,7 @@ import ProgramCard from "@/components/PageComponents/ProgramCard";
 import SCHOOLDATA from "../../../data/schooldata.json";
 import HeaderNoImage from "@/components/PageComponents/HeaderNoImage";
 
-const allprogrammes = () => {
+const Allprogrammes = () => {
   // const stumpNames = SCHOOLDATA.map((school) => school.nameStump);
   // console.log(stumpNames);
   const [schoolFilters, setSchoolFilters] = useState({
@@ -267,4 +267,4 @@ Allprogrammes.getLayout = function getLayout(page) {
   return <Layout1>{page}</Layout1>;
 };
 
-export default allprogrammes;
+export default Allprogrammes;
