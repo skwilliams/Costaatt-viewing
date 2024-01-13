ec4214a293a228fb1ec718c2d68836a0f6f6d19e
skwilliams <williamskstacy@gmail.com>
2024-01-12 22:03:14 -0400

    schoold

--- src/pages/programmes/schools/schooldata.js
+++ src/pages/programmes/schools/schooldata.js
@@ -1,3 +1,12 @@
+import React from 'react'
+
+const schooldata = () => {
+  return (
+    <div>schooldata</div>
+  )
+}
+
+export default schooldata
 export const schools = [
   {
     name: "School of Business and Digital Technologies",
