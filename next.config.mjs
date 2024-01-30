7aafd3af930cce15db59d800731927a843416ebc
skwilliams <williamskstacy@gmail.com>
2024-01-29 21:51:30 -0400

    mixin removed

--- next.config.mjs
+++ next.config.mjs
@@ -9,29 +9,19 @@ const nextConfig = {
       fs: false,
       path: false,
     };
-    // config.module.rules.push({
-    //   test: /\.(png|svg|jpe?g|gif|pdf)$/i,
-    //   use: [
-    //     {
-    //       loader: 'file-loader',
-    //       options: {
-    //         name: '[name].[ext]',
-    //       },
-    //     },
-    //   ],
-    // });
+   
     return config;
   },
 
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": [
-        "node_modules/@swc/core-linux-x64-gnu",
-        "node_modules/@swc/core-linux-x64-musl",
-        "node_modules/@esbuild/linux-x64",
-      ],
-    },
-  },
+  // experimental: {
+  //   outputFileTracingExcludes: {
+  //     "*": [
+  //       "node_modules/@swc/core-linux-x64-gnu",
+  //       "node_modules/@swc/core-linux-x64-musl",
+  //       "node_modules/@esbuild/linux-x64",
+  //     ],
+  //   },
+  // },
   sassOptions: {
     // includePaths: [path.join(__dirname, "styles")],
     prependData: `@use "~/styles/abstracts/mixins.scss";`,
