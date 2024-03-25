a05196879d0b60d56b7a7cc668782da5ac1a120a
skwilliams <williamskstacy@gmail.com>
2024-03-25 08:22:53 -0400

    area

--- src/pages/programmes/school.js
+++ src/pages/programmes/school.js
@@ -2,36 +2,26 @@ import TopNavOnlyLayout from "../../components/Layouts/TopNavOnly_NoDropdown";
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import standardStyles from "../../styles/main.module.scss"
+import standardStyles from "../../styles/main.module.scss";
 import ProgStyles from "../../styles/Programmes.module.scss";
 import HeaderNoImage from "../../components/PageComponents/HeaderNoImage";
 import { fetchCampus } from "../../lib/dataquery";
 import Image from "next/image";
-
 
 import { sql } from "@vercel/postgres";
 
 import HeadImage from "../../components/PageComponents/HeadImage";
 import awaitingbanner from "../../../public/images/about/qualitycontrol1.jpg";
 
-
-
-
-
 export async function getStaticProps() {
   // Fetch data from external API
-  const response = await sql`SELECT * FROM campus;`;
-  const campuses =  response.rows;
+  const response = await sql`SELECT * FROM school;`;
+  const schools = response.rows;
   // Pass data to the page via props
-  return { props: { campuses, } ,}
+  return { props: { schools } };
 }
 
-        
-
-const  visitus = ({campuses}) => {
-
-
-
+const visitus = ({ schools }) => {
   return (
     <>
       <Head>
@@ -44,45 +34,32 @@ const  visitus = ({campuses}) => {
         <Link href="/admissions/visitus">Visit Us</Link>
       </p>
       <div>
-      
-
         <div className={standardStyles.row}>
-          
-          
-            {campuses.map((campus) => {
-
-              return (
-                <>
-                  <div className={standardStyles.col1of3}>
-                    <div key={campus.id}>
-                      <ul>
-                        <li>
-                          {" "}
-                          <h2> {campus.campus_name}</h2>{" "}
-                        </li>
-                        <li>{campus.address} </li>
-                        <li>{campus.city} </li>
-                        </ul> 
-                        <Image 
-                             src= {campus.react_local_image}
-                        alt="Image of campus"
-                        width={300}
-                        height={300}
-                        />
-                        
-                     
-                    </div>
+          {schools.map((school) => {
+            return (
+              <>
+                <div className={standardStyles.col1of2}>
+                  <div key={school.id}>
+                    <ul>
+                      <li>
+                        {" "}
+                        <h2> {school.school_name}</h2>{" "}
+                      </li>
+                      {/* <li>{school.address} </li>
+                        <li>{campus.city} </li> */}
+                    </ul>
+                    <Image
+                      src={school.school_image}
+                      alt="Image of campus"
+                      width={200}
+                      height={200}
+                    />
                   </div>
-                </>
-              );
-          
+                </div>
+              </>
+            );
           })}
-         
-
-      
         </div>
-
-    
       </div>
     </>
   );
@@ -91,4 +68,4 @@ visitus.getLayout = function getLayout(page) {
   return <TopNavOnlyLayout> {page} </TopNavOnlyLayout>;
 };
 
-export  default visitus;
+export default visitus;
